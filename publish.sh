966dff37d019e9aeb573b1493f1957851574399c
Gabriel Dunne <gdunne@quilime.com>
2020-08-07 23:06:11 -0700

    Updating publish

--- publish.sh
+++ publish.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # delete static folder
-ssh sift@elev.us "cd sift.elev.us/public/ && rm -rf static"
+# ssh sift@elev.us "cd sift.elev.us/public/ && rm -rf static"
 
 # sync new build
-rsync -var --exclude "./public/static/" ./build/ sift@elev.us:sift.elev.us/public/
+rsync -var --delete --exclude ".DS_Store" --exclude "static" ./build/ sift@elev.us:sift.elev.us/public/
