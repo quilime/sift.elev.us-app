f6b89488f08a6b7060f987fa9641c6c8c49da167
Gabriel Dunne <gdunne@quilime.com>
2020-08-07 23:10:30 -0700

    Updating publish to skip

--- publish.sh
+++ publish.sh
@@ -5,3 +5,4 @@ ssh sift@elev.us "cd sift.elev.us/public/ && rm -rf precache-*"
 
 # sync new build
 rsync -var --exclude "static/sift" --exclude ".DS_Store" ./build/ sift@elev.us:sift.elev.us/public/
+
