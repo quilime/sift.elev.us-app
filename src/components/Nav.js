64a868b4296d48a6b2d444ff11f33cf5b471b76f
Gabriel Dunne <gdunne@quilime.com>
2020-08-27 21:56:23 -0700

    Iconifying menu

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,112 +1,63 @@
-import React, { useState } from 'react';
-import { NavLink, withRouter } from "react-router-dom";
+import React, { } from 'react';
+import { withRouter } from "react-router-dom";
 import styled from 'styled-components';
 import { useSelector, useDispatch } from "react-redux";
-import { Button, Radio } from 'antd';
-import { MenuOutlined } from '@ant-design/icons';
+import { Button, Tooltip } from 'antd';
+import { 
+  SettingOutlined, 
+  UploadOutlined, 
+  HeatMapOutlined,
+  PlusOutlined,
+  MinusOutlined
+} from '@ant-design/icons';
 
 
-const ResponsiveMenu = styled.div`
-  width: 2rem;
-  height: 2rem;
+const NavDiv = styled.div`
   position: fixed;
   top: 15px;
   left: 15px;
   z-index: 20;
 
-  & .anticon-menu {
-      display:none;
-  }
-
-  .anticon-menu {
-    display: ${({ open }) => open ? 'none' : 'block'};
-  }
-
-  & .nav {
-    display: ${({ open }) => open ? 'block' : 'none'};
-    position: fixed;
-    top: 0;
-    left: 0;
-    width:150px;
-    z-index:1000;
-    background:#fff;
-    padding:0.5em;
-  }
-
-  ul {
-    padding:0;
-    margin:0;
-    padding-bottom:1em;
-
-
-    & a:first-child {
-      margin-top:0.5em;
-      letter-spacing: 0.5em;
-    }
-    & a, & a.strong {
-      display: block;
-      font-weight:bold;
-      border-bottom: 2px solid #ddd;
-    }
-    & a {
-      margin:2em 0;
-      display:block;
-    }
-    & a.active, a.active {
-      border-bottom: 3px solid #000 !important;
-    }
+  & .ant-btn {
+    display: block;
+    margin-bottom: 0.5em;
   }
 `;
 
 
-
-
-
-const radioStyle = {
-  display: 'inline-block',
-  height: '30px',
-  width: '30px',
-  padding: '2px',
-  textAlign: 'center',
-  fontWeight: 'bold'
-};
-
-const radioGroupStyle = {
-  margin: '0 0 0 2em'
-};
-
-
 const Nav = (props) => {
-
-  const [open, setOpen] = useState(false);
 
   const dispatch = useDispatch();
   const imageViewSize = useSelector(state => state.reducers.imageViewSize);
 
-  const onViewChange = e => {
-    dispatch({ type: 'SET_IMAGEVIEWSIZE', imageViewSize: e.target.value });
-  };
-
-  const navClick = () => {
-    setOpen(false);
+  const setView = (e, val) => {
+    dispatch({ type: 'SET_IMAGEVIEWSIZE', imageViewSize: val });
   }
 
   return(
-    <ResponsiveMenu open={open}>
-      <MenuOutlined onClick={() => setOpen(!open)} />
-      <div className="nav">
-        <ul onClick={navClick}>
-          <NavLink to={`/`} exact>S I F T</NavLink>
-          <NavLink to={`/upload`} exact>upload</NavLink>
-          <NavLink to={`/settings`} exact>settings</NavLink>
-        </ul>
-        <Radio.Group style={radioGroupStyle} defaultValue={imageViewSize} size="small" onChange={onViewChange}>
-          <Radio.Button style={radioStyle} value="large">+</Radio.Button>
-          <Radio.Button style={radioStyle} value="compact">-</Radio.Button>
-        </Radio.Group>
-        <Button onClick={navClick}>close</Button>
-      </div>
-    </ResponsiveMenu>
+    <NavDiv>
+
+      <Tooltip placement="right" title="S I F T" className="icon">
+        <Button shape="circle" icon={<HeatMapOutlined />} href="/" />
+      </Tooltip>
+
+      <Tooltip placement="right" title="Upload" className="icon">
+        <Button shape="circle" icon={<UploadOutlined />} href="/upload" />
+      </Tooltip>
+
+      <Tooltip placement="right" title="Settings" classname="icon">
+        <Button shape="circle" icon={<SettingOutlined />} href="/settings" />
+      </Tooltip>
+
+      <Tooltip placement="right" title="Large Images" classname="icon">
+        <Button disabled={ imageViewSize === "small" ? false : true } shape="circle" icon={<PlusOutlined />} onClick={(e) => setView(e, "large") } />
+      </Tooltip>
+
+      <Tooltip placement="right" title="Small Images" classname="icon">
+        <Button disabled={ imageViewSize === "small" ? true : false } shape="circle" icon={<MinusOutlined />} onClick={(e) => setView(e, "small") } />
+      </Tooltip>
+
+    </NavDiv>
   );
 };
 
